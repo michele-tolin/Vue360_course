7f6005d63162cc069f8c155f9d889ac8eb5eb20d
michele.tolin <michele.tolin@4-flying.com>
2020-05-28 02:58:33 +0200

    second commit

--- week_1/src/components/MainBox.vue
+++ week_1/src/components/MainBox.vue
@@ -2,38 +2,45 @@
   <div>
     <!--Form-->
     <div
-      class="w-1/2 m-auto p-6 shadow flex flex-col justify-between bg-blue-300"
+      class="w-1/2 m-auto p-6 shadow flex flex-col justify-between bg-blue-100 border border-blue-200 rounded"
     >
       <form>
-        <div class="flex justify-center items-center">
+
+        <div class="flex flex-wrap justify-center items-center">
+          <label class="w-full pb-3" for="textMessage">Insert your message below</label>
           <textarea
             name="textMessage"
-            class="border border-blue-400 focus:border-none shadow px-3 py-2 mr-3"
+            class="w-full border border-blue-400 focus:border-none shadow px-3 py-2 mb-3 rounded"
             v-model.trim="messageToSubmit"
           ></textarea>
-          <button
-            class="px-3 py-2 border rounded bg-blue-800 text-blue-100 border-gray-500 hover:bg-blue-900 hover:text-gray-200"
-            :disabled="!canSubmit()"
-            :class="{
-              'cursor-not-allowed': !canSubmit()
-            }"
-            @click.prevent="addMessage"
-          >
-            Submit
-          </button>
+
+          <div class="w-full flex justify-end  items-center mb-3">
+            <div
+              class=" "
+              :class="{
+                'font-bold text-red-700': !canSubmit()
+              }"
+            >
+              {{ countCharacters() }} character
+              <span v-if="Math.abs(countCharacters()) > 1" >
+                s
+              </span> left
+            </div>
+            <button
+              class="px-3 py-2 ml-3 border rounded bg-blue-600 text-blue-100 border-blue-800 hover:bg-blue-900 hover:text-gray-200"
+              :disabled="!canSubmit()"
+              :title="getButtonTitle()"
+              :class="{
+                'cursor-not-allowed opacity-25': !canSubmit()
+              }"
+              @click.prevent="addMessage"
+            >
+              Submit
+            </button>
+          </div>
         </div>
       </form>
-      <div
-        class="font-bold mt-3"
-        :class="{
-          'text-red-500': !canSubmit()
-        }"
-      >
-        {{ countCharacters() }} of {{ textLimit }} character
-       <span v-if="Math.abs(countCharacters()) > 1" >
-          s
-       </span> left
-      </div>
+
     </div>
 
     <!--List-->
@@ -41,10 +48,14 @@
       class="w-1/2 m-auto mt-6" v-if=" hasMessages() ">
       <div class="py-2 font-bold ">Latest messages</div>
       <div class="">
-        <div class="shadow py-2 mb-2 shadow" v-for="message in messageList" :key="message.id">
 
-          {{ message.text }} @ {{ message.time }}
+        <div class="shadow py-2 px-3 mb-2 shadow overflow-auto" v-for="message in messageList" :key="message.id">
+          <div>Posted @ {{ message.time }}</div>
+          <textarea class="w-full" readonly :value="message.text">
+
+          </textarea>
         </div>
+
       </div>
     </div>
   </div>
@@ -56,7 +67,7 @@ export default {
 
   data() {
     return {
-      textLimit: 10,
+      textLimit: 255,
       messageToSubmit: "",
       messageList: []
     };
@@ -72,6 +83,15 @@ export default {
     canSubmit() {
       return this.countCharacters() >= 0;
     },
+
+    getButtonTitle() {
+      var message = "click to submit";
+      if(this.countCharacters()<0) {
+          message = "Exceeeding by " + Math.abs(this.countCharacters()) + " characters";
+      }
+      return message;
+    },
+
     addMessage() {
 
       if(!this.canSubmit()) {
