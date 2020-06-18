1eaadfb6c1e2db4b1fc641c1bead1b22bc9de5a3
Michele <51199355+michele-tolin@users.noreply.github.com>
2020-06-18 02:35:39 +0200

    rollback

--- week_4/expensetracker/src/main.ts
+++ week_4/expensetracker/src/main.ts
@@ -16,13 +16,13 @@ Vue.config.productionTip = false;
 Vue.prototype.$axios = axios;
 
 const firebaseConfig = {
-  apiKey: "***",
-  authDomain: "****",
-  databaseURL: "***",
-  projectId: "***",
-  storageBucket: "***m",
-  messagingSenderId: "***8",
-  appId: "****",
+  apiKey: "AIzaSyCH6b7nf5GnznQD2PQliszX-xlnQIIihjI",
+  authDomain: "xpence-vue.firebaseapp.com",
+  databaseURL: "https://xpence-vue.firebaseio.com",
+  projectId: "xpence-vue",
+  storageBucket: "xpence-vue.appspot.com",
+  messagingSenderId: "1001066676608",
+  appId: "1:1001066676608:web:7e2b56962002e8d767c165",
 };
 
 firebase.initializeApp(firebaseConfig);
